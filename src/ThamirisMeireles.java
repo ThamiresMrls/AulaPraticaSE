c28ae5cef8e8cd13f81fcfa91681f6986970acb7
gessica.monique <gessica.monique@LAB2-DCX-27.dcx.ufpb.br>
2017-09-11 11:19:08 -0300

    Commit código Thami

--- src/ThamirisMeireles.java
+++ src/ThamirisMeireles.java
@@ -4,6 +4,8 @@ public class ThamirisMeireles {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Thamiris Meirles");
+		
+		System.out.println("Géssica clonou!");
 
 	}
 
